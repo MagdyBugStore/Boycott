6ff46099144e3ba0c4d98ad6d4810d0cef12f00d
Ahmed magdy <45017562+MagdyBugStore@users.noreply.github.com>
2024-09-19 13:34:56 +0300

    handle Bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ProductCard from '@components/ProductCard';
+import ProductCard from './components/productCard';
 import list from './static/data.json';
 
 const App = () => {
